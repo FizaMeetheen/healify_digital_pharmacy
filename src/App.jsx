fbac716d92a8ff4e5e5f82a3ae2714489f54f41c
amjads06 <amjadshaji06@gmail.com>
2025-10-30 23:06:23 +0530

    changes added by amjad

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
+import LandingPage from './pages/LandingPage'
+import Home from './pages/Home'
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
     <>
     
      <Routes>
+      <Route path="/" element={<LandingPage/>}/>
+      <Route path="/home" element={<Home/>}/>
       <Route path="/cart" element={<Cart/>}/>
       <Route path="/checkout" element={<Checkout/>}/>
      </Routes>
