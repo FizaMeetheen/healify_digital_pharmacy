61807aacf699f0180d638348f49566916d5f547d
chinju03 <chinjuann03@gmail.com>
2025-11-01 12:00:23 +0530

    fea:cart checkout

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 function Cart() {
     return (
@@ -50,6 +51,8 @@ function Cart() {
                     </div>
                 </div>
             </div>
+
+            <Footer/>
         </>
     )
 }
